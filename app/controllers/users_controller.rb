ed3c8482f08f307f9a6ff0e302350f365581f1a7
namitop <nariyasuhidetaka@gmail.com>
2016-05-17 06:01:36 +0000

    lesson7-7 task on user-profile branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -17,9 +19,27 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to user_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :residence, :detail)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
